9467a7237c39b9bfa86ab77002ab0589194556e6
RakotoGisele <67727395+RakotoGisele@users.noreply.github.com>
2020-11-29 13:24:40 +0400

    dossier de réservation v2

---
+++ café_réservation/javascript.js
@@ -0,0 +1,54 @@
+
+
+setInterval(function(){
+    var d = new Date(); 
+    var h = d.toLocaleTimeString();
+    console.log(d.toLocaleTimeString()); 
+    document.getElementById("date").innerHTML=h;
+},1000)
+
+function myFunction() {
+    document.getElementById("situation").innerHTML="Indisponible";
+}
+function couleur() {
+    var element =  document.getElementById("situation");
+    element.classList.remove("bouton");
+    var element =  document.getElementById("situation");
+    element.classList.add("indisponible");
+
+}
+
+function defaut() {
+    var element =  document.getElementById("situation");
+    element.classList.add("bouton");
+    var element =  document.getElementById("situation");
+    element.classList.remove("indisponible");    
+    document.getElementById("situation").innerHTML="Disponible";
+    document.getElementById("utilisation").innerHTML="Reserver un Ordinateur ";
+}
+
+function timer1() {
+    var myVar = setInterval(myTimer ,100);
+    var d = new Date();
+    document.getElementById("utilisation").innerHTML = d.toLocaleTimeString();
+    document.getElementById("utilisation").innerHTML="Indisponible j'usque"+d;
+}
+
+
+function timer2() {
+    
+}
+
+
+function timer3() {
+    
+}
+function timer4() {
+    
+}
+function timer5() {
+    
+}
+function timer6() {
+    
+}
